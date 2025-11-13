7f6062cf746af601d1c88ecaedb35bac735eb97b
Mirage <cognitiveorganism@gmail.com>
2025-11-13 05:06:35 +0100

    Update MirageToken.sol

--- MirageToken.sol
+++ MirageToken.sol
@@ -1,1 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
 
+import "@openzeppelin/contracts/token/SPL/SPL.sol";
+import "@openzeppelin/contracts/token/SPL/extensions/SPLBurnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Arystoe is SPL, SPLBurnable, Pausable, Ownable {
+    constructor() SPL("Arystoe", "LMR") {
+        _mint(msg.sender, 1000000000 * 10 ** decimals());
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 8;
+    }
+
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+}
